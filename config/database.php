8027f82c7ecd9ee132988d8fef423af1fd33d831
Love Chauhan <lovechn1407@gmail.com>
2026-09-11 18:27:43 +0530

    perf: enable persistent PDO connections for serverless container reuse

--- config/database.php
+++ config/database.php
@@ -67,6 +67,7 @@ return [
             'sslmode' => 'prefer',
             'options' => [
                 \PDO::ATTR_EMULATE_PREPARES => true,
+                \PDO::ATTR_PERSISTENT => true,
             ],
         ],
 
